640efcce7dd6f582392b35d96da950b669029391
Koprvhdix <454898060@qq.com>
2016-03-21 22:11:30 +0800

    db first commit

---
+++ db/item_set.py
@@ -0,0 +1,5 @@
+item_set = []
+
+for i in range(10000):
+    id = 'Item' + str(i)
+    item_set.append(id)
---
+++ db/user_set.py
@@ -0,0 +1,9 @@
+user_set = []
+
+for i in range(100):
+    name = 'U' + str(i)
+    user_set.append(name)
+
+# Unit test
+'''for item in user_set:
+    print(item)'''
